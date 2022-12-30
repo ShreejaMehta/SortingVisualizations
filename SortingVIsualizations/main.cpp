894f8bfe65038a78b1239cdec5eef4faec41b7f9
ShreejaMehta <shreejamehta1609@gmail.com>
2022-12-30 09:18:51 +0530

    modified:   SortingVIsualizations.xcodeproj/project.pbxproj 	modified:   SortingVIsualizations/main.cpp

--- SortingVIsualizations/main.cpp
+++ SortingVIsualizations/main.cpp
@@ -13,17 +13,60 @@
 // application bundle. To get the path to these resources, use the helper
 // function `resourcePath()` from ResourcePath.hpp
 //
-
-#include <SFML/Audio.hpp>
 #include <SFML/Graphics.hpp>
+#include<unistd.h>
 
 // Here is a small helper for you! Have a look.
 #include "ResourcePath.hpp"
 
+sf::RenderWindow window(sf::VideoMode(960, 600), "Insertion Sort");
+int n=80;
+float recHs[80];
+unsigned int microsecond = 1000000;
+bool sorted=false;
+
+void dispSort(int index){
+    window.clear();
+    for(int i=0; i<n; i++){
+        sf::RectangleShape block(sf::Vector2f(10, recHs[i]));
+        block.setPosition(i*12, 600-recHs[i]);
+        block.setFillColor(sorted || i==index? sf::Color::Green : sf::Color::White);
+        window.draw(block);
+    }
+    
+    window.display();
+}
+
+void insertionSort()
+{
+    usleep(microsecond*5);
+    int i, key, j;
+    for (i = 0; i < n; i++)
+    {
+        key = recHs[i];
+        j = i - 1;
+
+        while (j >= 0 && recHs[j] > key)
+        {
+            recHs[j + 1] = recHs[j];
+            j = j - 1;
+            dispSort(j);
+        }
+        recHs[j + 1] = key;
+    }
+
+    sorted = true;
+
+    dispSort(i);
+
+}
+
 int main(int, char const**)
 {
-    // Create the main window
-    sf::RenderWindow window(sf::VideoMode(800, 600), "SFML window");
+    for(int i=0; i<n; i++){
+            recHs[i]=(rand()%500);
+        }
+
     // Start the game loop
     while (window.isOpen())
     {
@@ -42,10 +85,17 @@ int main(int, char const**)
                 window.close();
             }
         }
+        
+        if(!sorted){
+                    dispSort(0);
+                    insertionSort();
+                }
 
-        window.clear();
-        window.display();
+//        window.clear();
+//        window.display();
     }
 
-    return EXIT_SUCCESS;
+    return ;
 }
+
+
