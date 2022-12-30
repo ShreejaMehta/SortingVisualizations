e23d3ecb5e055bb6618273e2a8ed162974e76752
ShreejaMehta <shreejamehta1609@gmail.com>
2022-12-30 08:37:44 +0530

    Successful window rendering

--- SortingVIsualizations/main.cpp
+++ SortingVIsualizations/main.cpp
@@ -24,41 +24,10 @@ int main(int, char const**)
 {
     // Create the main window
     sf::RenderWindow window(sf::VideoMode(800, 600), "SFML window");
-
-    // Set the Icon
-    sf::Image icon;
-    if (!icon.loadFromFile(resourcePath() + "icon.png")) {
-        return EXIT_FAILURE;
-    }
-    window.setIcon(icon.getSize().x, icon.getSize().y, icon.getPixelsPtr());
-
-    // Load a sprite to display
-    sf::Texture texture;
-    if (!texture.loadFromFile(resourcePath() + "cute_image.jpg")) {
-        return EXIT_FAILURE;
-    }
-    sf::Sprite sprite(texture);
-
-    // Create a graphical text to display
-    sf::Font font;
-    if (!font.loadFromFile(resourcePath() + "sansation.ttf")) {
-        return EXIT_FAILURE;
-    }
-    sf::Text text("Hello SFML", font, 50);
-    text.setFillColor(sf::Color::Black);
-
-    // Load a music to play
-    sf::Music music;
-    if (!music.openFromFile(resourcePath() + "nice_music.ogg")) {
-        return EXIT_FAILURE;
-    }
-
-    // Play the music
-    music.play();
-
     // Start the game loop
     while (window.isOpen())
     {
+        window.setFramerateLimit(60);
         // Process events
         sf::Event event;
         while (window.pollEvent(event))
@@ -74,16 +43,7 @@ int main(int, char const**)
             }
         }
 
-        // Clear screen
         window.clear();
-
-        // Draw the sprite
-        window.draw(sprite);
-
-        // Draw the string
-        window.draw(text);
-
-        // Update the window
         window.display();
     }
 
